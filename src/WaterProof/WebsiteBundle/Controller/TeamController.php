0bd24179db3e728df5bf767a5f9f068b480358c4
jdubois <jdubois@waterproof.fr>
2014-02-07 16:40:05 +0100

    first commit

---
+++ src/WaterProof/WebsiteBundle/Controller/TeamController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WaterProof\WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+
+class TeamController extends Controller
+{
+
+	/**
+	 * @Route("/montauban/membres", name="team")
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		return array();
+	}
+	/**
+	 * @Route("/montauban/membres/shordeaux", name="team_shordeaux")
+	 * @Template()
+	 */
+	public function shordeauxAction()
+	{
+		return array();
+	}
+
+	/**
+	 * @Route("/montauban/membres/cbarragan", name="team_cbarragan")
+	 * @Template()
+	 */
+	public function cbarraganAction()
+	{
+		return array();
+	}
+
+	/**
+	 * @Route("/montauban/membres/jdubois", name="team_jdubois")
+	 * @Template()
+	 */
+	public function jduboisAction()
+	{
+		return array();
+	}
+
+
+}
---
+++ src/WaterProof/WebsiteBundle/WaterProofWebsiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WaterProof\WebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WaterProofWebsiteBundle extends Bundle
+{
+}
